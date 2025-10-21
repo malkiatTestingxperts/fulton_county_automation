04876325ba045d43f4877c6b77f50fd0fbf6e8e5
malkiatTestingxperts <malkiat.singh@testingxperts.com>
2025-10-21 12:23:37 +0530

    first commit

---
+++ tests/test.spec.ts
@@ -0,0 +1,9 @@
+import { test } from "@playwright/test";
+import { BrowserManager } from "@tx/playwright-core";
+import { click } from "@tx/playwright-core";
+
+test('Google test', async () => {
+    const page = await BrowserManager.getPage();
+    await page.goto('https://google.com');
+    await click('[role="combobox"]');
+});
