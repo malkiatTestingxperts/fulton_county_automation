854ee9c8b176bc23048e2f2fdaaed0c530a59c6f
malkiatTestingxperts <malkiat.singh@testingxperts.com>
2025-10-21 13:17:34 +0530

    publish the master branch for fulton county

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -5,5 +5,6 @@ import { click } from "@tx/playwright-core";
 test('Google test', async () => {
     const page = await BrowserManager.getPage();
     await page.goto('https://google.com');
+    // to test the click fuction
     await click('[role="combobox"]');
 });
