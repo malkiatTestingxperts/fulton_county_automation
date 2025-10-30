4cffa071894a03aa032b45f94eaa2b9041154548
malkiatTestingxperts <malkiat.singh@testingxperts.com>
2025-10-30 13:02:10 +0530

    Updated this repo

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,7 +1,10 @@
-import { test } from '@playwright/test';
-import { launchURL, click } from '@tx/playwright-core';
+import { test } from "@playwright/test";
+import { launchURL, logger, findElementUniversal } from "@tx/playwright-core";
 
-test('login', async ({ page }) => {
-    await launchURL(page, 'https://google.com');
-    await click(page, '[role="combobox"]',);
+test("login", async ({ page }) => {
+    await launchURL(page, "https://google.com");
+    const searchBox = await findElementUniversal(
+        page, ['input[name="3q1"]', '[role="csasombobox1"]'], "Search Box"
+    );
+    await searchBox.click();
 });
